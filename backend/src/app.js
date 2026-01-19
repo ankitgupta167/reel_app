88ff1a8620de464a66013d9b98e143d8dfe53951
ankitgupta167 <ankitpgupta051@gmail.com>
2026-01-19 16:36:39 +0530

    putting frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,8 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173",
+       " https://reel-app-beta.vercel.app/"],
     credentials: true
 }));
 app.use(cookieParser());
